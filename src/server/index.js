d709429147f0d399a57fa148bcdfdd41e7f2b5b9
QiLinXue <la.qcumber@gmail.com>
2018-09-15 21:22:30 -0400

    better one

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,7 @@ app.use(express.static("dist"));
 
 ////
 
-// const sampleAudio = ""./data/test4.wav"";
+// const sampleAudio = ""./data/test2.wav"";
 
 ///////////////////////////////////////////////
 // Azure
@@ -70,7 +70,7 @@ app.get("/api/transcribeSample", (req, res) => {
       console.log("recognizer is stopped.");
     });
 
-    await recognizer.sendFile("./data/test4.wav");
+    await recognizer.sendFile("./data/test2.wav");
     console.log("file sent.");
   })();
 });
@@ -216,7 +216,7 @@ app.get("/api/getEmotions", (req, res) => {
 app.get("/api/getAudioLength", (req, res) => {
   var wavFileInfo = require("wav-file-info");
 
-  wavFileInfo.infoByFilename("./data/test4.wav", function(err, info) {
+  wavFileInfo.infoByFilename("./data/test2.wav", function(err, info) {
     if (err) throw err;
     res.send({ audioLength: info.duration });
   });
@@ -229,7 +229,7 @@ app.get("/api/getWPM/:text", (req, res) => {
 
   var wavFileInfo = require("wav-file-info");
 
-  wavFileInfo.infoByFilename("./data/test4.wav", function(err, info) {
+  wavFileInfo.infoByFilename("./data/test2.wav", function(err, info) {
     if (err) throw err;
     length = info.duration;
     words = inputText.split(" ").length;
